809c5de906ac15595f3bc1988868749af2993449
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2023-05-29 12:43:13 -0300

    feat(logs): implementa a lib de logs

---
+++ libs/logger.py
@@ -0,0 +1,15 @@
+import logging
+from sys import stdout
+
+
+def log(module_name: str):
+    handler_stdout = logging.StreamHandler(stdout)
+    handler_stdout.setLevel(logging.DEBUG)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[handler_stdout],
+    )
+    return logging.getLogger(module_name)
