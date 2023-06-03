6a012a9980ca0c6127bb36c2414370a9c8428682
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2023-06-02 22:27:14 -0300

    fix(circle): corrige bug ao gerar pontos com centro fora de 0,0

--- algorithms/circle.py
+++ algorithms/circle.py
@@ -17,17 +17,25 @@ class Circle(BaseAlgorithm):
             return
 
         center = selected_cells[-1]
-        base_points = self.__get_base_points(center, int(parameters['Raio']))
+        base_points = self.__get_base_points(int(parameters['Raio']))
         points = self.__get_circle_points(center, base_points)
 
         for cell in points:
             self.grid.render_cell(cell)
 
-    def __get_base_points(
-        self, center: tuple[int, int], radius: int
-    ) -> list[tuple[int, int]]:
-        x_c, _ = center
-        x, y = x_c, radius
+    def __get_base_points(self, radius: int) -> list[tuple[int, int]]:
+        """
+        Obtem os pontos base, que serão utlizados para gerar os pontos em cada
+        uma dos octantes.
+
+        Parameters:
+            radius (int): Raio do círculo.
+
+        Returns:
+            base_points (list): Uma lista de tuplas, com as coordenadas dos
+            pontos base.
+        """
+        x, y = 0, radius
         err = -radius
         base_points = []
 
@@ -43,15 +51,25 @@ class Circle(BaseAlgorithm):
                 y -= 1
             base_points.append((x, y))
 
-        # TODO:
-        # - [ ] partir de outras posições para o centro, está sempre em (0,0)
-
         self.log.info(f'Pontos base {base_points}')
         return base_points
 
     def __get_circle_points(
-        self, center: tuple[int, int], base_points: list[tuple[int, int]]
+        self, center: tuple, base_points: list[tuple[int, int]]
     ) -> list[tuple[int, int]]:
+        """
+        A partir de uma lista de coordenadas, dos pontos base, gera seus pontos espelhados nos 8
+        octantes. Utiliza o espelhamento entre os octantes.
+
+        Parameters:
+            center (tuple): Tupla com as coordenadas do centro do círculo.
+
+            base_points (list): Lista com os pontos que serviram como base para
+            geração dos pontos correspondentes nos 8 octantes.
+
+        Returns:
+            points (list): Lista de pontos espelhados nos 8 octantes.
+        """
         x_c, y_c = center
         points = []
 
@@ -64,21 +82,21 @@ class Circle(BaseAlgorithm):
             for octant in range(8):
                 match octant:
                     case 0:
-                        points.append((y + y_c, x + x_c))
+                        points.append((y + x_c, x + y_c))
                     case 1:
                         points.append((x + x_c, y + y_c))
                     case 2:
                         points.append((-x + x_c, y + y_c))
                     case 3:
-                        points.append((-y + y_c, x + x_c))
+                        points.append((-y + x_c, x + y_c))
                     case 4:
-                        points.append((-y + y_c, -x + x_c))
+                        points.append((-y + x_c, -x + y_c))
                     case 5:
                         points.append((-x + x_c, -y + y_c))
                     case 6:
                         points.append((x + x_c, -y + y_c))
                     case 7:
-                        points.append((y + y_c, -x + x_c))
+                        points.append((y + x_c, -x + y_c))
                 self.log.info(f'{octant+1}° octante {points[-1]} ')
 
         if self.debug:
